bb31e8bf8ef3234ea4b2bab537fc27078d07488f
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2023-05-06 10:28:29 +0300

    Changed the name of the hide class to hidden

--- src/conponents/HintsRow.js
+++ src/conponents/HintsRow.js
@@ -5,7 +5,7 @@ class HintsRow extends React.Component {
         const { leftHint, rightHint, activeEmotion} = this.props;
         const emotion = leftHint.charAt(0).toUpperCase() + leftHint.slice(1) + rightHint.charAt(0).toUpperCase() + rightHint.slice(1);
 
-        return (<div className={`hintsRow ${activeEmotion === emotion ? "" : "hide"}`}>
+        return (<div className={`hintsRow ${activeEmotion === emotion ? "" : "hidden"}`}>
             <div className="hint">{this.props.leftHint}</div>
             <div className="hint">{this.props.rightHint}</div>
         </div>)
